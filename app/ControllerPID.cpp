b056573bb56b967eb0efa58aacd3169a8cd0c00c
lilnpuma <manump@terpmail.umd.edu>
2022-10-01 14:11:58 -0400

    Updated class definitions according to UML diagram, added missing files to app/CmakeLists.txt

--- app/ControllerPID.cpp
+++ app/ControllerPID.cpp
@@ -1,6 +1,6 @@
 /**
  * @file ControllerPID.cpp
- * @author Manu Madhu Pillai (manump@umd.edu)
+ * @author Manu Madhu Pillai (manump@umd.edu) (Driver) and Arshad Shaik (Navigator)
  * @brief PID controller for robot control
  * @version 0.1
  * @date 2022-09-30
@@ -9,12 +9,29 @@
  * 
  */
 #include "ControllerPID.hpp"
+#include <tuple>
 
 using namespace PID;
 
-ControllerPID::ControllerPID(){
+ControllerPID::ControllerPID(double p, double i, double d)
+    :kp{p}, ki{i}, kd{d} {
 
 }
-ControllerPID::~ControllerPID(){
 
+ControllerPID::ControllerPID(){
+}
+
+ControllerPID::~ControllerPID(){
+}
+
+double ControllerPID::compute(double velocity_target, double velocity_current){
+    return 0.0;
+}
+
+std::tuple<double, double, double> ControllerPID::getGains(){
+    return std::tuple<double, double, double>(kp, ki, kd);
+}
+
+double ControllerPID::getError{
+    return error;
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,6 @@
 /**
  * @file main.cpp
- * @author Manu Madhu Pillai (manump@umd.edu)
+ * @author Manu Madhu Pillai (manump@umd.edu) (Driver) and Arshad Shaik (arshad22@umd.edu) (Navigator)
  * @brief PID controller for robot control
  * @version 0.1
  * @date 2022-09-30
@@ -10,9 +10,23 @@
  */
 #include <iostream>
 #include <ControllerPID.hpp>
+#include <string>
 
-int main()
-{
-    dummy();
+int main(){
+    std::string target;
+    std::cout << " Input Target Velocity: ";
+    std::getline(std::cin, target);
+    PID::ControllerPID controller(3.0, 1.1, 0.0);
+    double velocity_current = 15.0;
+    auto velocity_target = std::stod(target);
+    std::cout <<" PID Values: "
+              <<std::get<0>(controller.getGains()) <<", "
+              <<std::get<1>(controller.getGains()) <<", "
+              <<std::get<2>(controller.getGains()) 
+              <<"\nCurrent Velocity: " << velocity_current
+              <<"\nTarget Velocity: " << velocity_target
+              <<"\nOutput Velocity: " << controller.compute(velocity_target, velocity_current)
+              <<"\nError: " << controller.getError();
+    
     return 0;
 }
