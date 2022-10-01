de0f2d24db67e2648b8c2308860374c06b53edc5
lilnpuma <manump@terpmail.umd.edu>
2022-10-01 14:49:12 -0400

    Added tests to validate the functionality of ControllerPID class.

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Manu Madhu Pillai (manump@umd.edu) (Driver) and Arshad Shaik (arshad22@umd.edu) (Navigator)
+ * @brief Runs tests described in the test file
+ * @version 0.1
+ * @date 2022-10-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,32 @@
+/**
+ * @file test.cpp
+ * @author Manu Madhu Pillai (manump@umd.edu) (Driver) and Arshad Shaik (arshad22@umd.edu) (Navigator)
+ * @brief Contains test to validate the functionality of the ControllerPID Class.
+ * @version 0.1
+ * @date 2022-10-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+ 
+ // Headers
 #include <gtest/gtest.h>
+#include <math.h>
+#include <tuple>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+// Class source of CPP file
+#include "ControllerPID.hpp"
+
+TEST(ControllerPIDTest, test_compute){
+  PID::ControllerPID controllertest(2.0, 0.0, 0.0);
+  EXPECT_NEAR(controllertest.compute(5.0, 7.0), -4.0, 0.0001);
+}
+TEST(ControllerPIDTest, test_getGains){
+  PID::ControllerPID controllertest;
+  EXPECT_EQ(controllertest.getGains(),
+            (std::tuple<double, double, double>(0.0, 0.0, 0.0)));
+}
+TEST(ControllerPIDTest, test_getError){
+  PID::ControllerPID controllertest(2.0, 0.0, 0.1);
+  EXPECT_EQ(controllertest.getError(), 0.0);
 }
